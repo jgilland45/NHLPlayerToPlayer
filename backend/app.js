f4502dc4a5fb81a9c9be60f423e1bb33929d40b8
Jason Gillanders <jgilland@ualberta.ca>
2024-01-21 14:49:33 -0700

    created a small multiplayer game using sockets

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 // To enable Vue CLI: https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system
-const Express = require("express")();
-const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
+const Express = require("express");
+var app = Express();
+const Http = require("http").Server(app);
+const Socketio = require("socket.io")(Http, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"],
+    }
+});
 
 var position = {
     x: 200,
@@ -10,6 +16,26 @@ var position = {
 
 Socketio.on("connection", socket => {
     socket.emit("position", position);
+    socket.on("move", data => {
+        switch (data) {
+            case "left":
+                position.x -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "right":
+                position.x += 5;
+                Socketio.emit("position", position);
+                break;
+            case "up":
+                position.y -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "down":
+                position.y += 5;
+                Socketio.emit("position", position);
+                break;
+        }
+    });
 });
 
 Http.listen(3000, () => {
--- client/src/components/BlockGame.vue
+++ client/src/components/BlockGame.vue
@@ -1,9 +1,45 @@
 <template>
   <div>
+    <canvas
+      ref="game"
+      width="640"
+      height="480"
+      style="border: 1px solid black;">
+    </canvas>
+    <div>
+      <button @click="move('right')">Right</button>
+      <button @click="move('left')">Left</button>
+      <button @click="move('up')">Up</button>
+      <button @click="move('down')">Down</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { io } from 'socket.io-client'
+
+const socket = ref(io('http://localhost:3000'))
+const context = ref()
+const game = ref()
+const position = ref({
+  x: 0,
+  y: 0
+})
+
+onMounted(() => {
+  context.value = game.value.getContext('2d')
+  socket.value.on('position', data => {
+    position.value = data
+    context.value.clearRect(0, 0, game.value.width, game.value.height)
+    context.value.fillRect(position.value.x, position.value.y, 20, 20)
+  })
+})
+
+const move = (direction) => {
+  socket.value.emit('move', direction)
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
