6e010cd51e59a15efa96d0c14b5a1e4da3ae647f
Jason Gillanders <jgilland@ualberta.ca>
2024-02-19 09:35:55 -0700

    Using puckdoku faces to show on screen

--- client/src/components/PlayerBlock.vue
+++ client/src/components/PlayerBlock.vue
@@ -4,17 +4,33 @@
             {{ props.player.name }}
         </div>
         <div class="player-headshot">
-            <img src="https://assets.nhle.com/mugs/nhl/default-skater.png" width="100px"/>
+            <img :src="imgSrc" width="100px"/>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, computed, defineProps, defineEmits } from 'vue'
+import { ref, watch, computed, defineProps, defineEmits } from 'vue'
 
 const props = defineProps({
   player: { name: String, url: String }
 })
+const getImgUrl = ref(true)
+const imgPartOfUrl = computed(() => props.player.url ? props.player.url.slice(11, -5) : '')
+const imgSrc = computed(() => getImgUrl.value !== false ? `https://www.puckdoku.com/faces/${imgPartOfUrl.value}.jpg` : 'https://assets.nhle.com/mugs/nhl/default-skater.png')
+
+watch(imgPartOfUrl, () => {
+  if (imgPartOfUrl.value === '') {
+    getImgUrl.value = false
+  } else {
+    // https://stackoverflow.com/questions/18837735/check-if-image-exists-on-server-using-javascript
+    const imgURL = `https://www.puckdoku.com/faces/${imgPartOfUrl.value}.jpg`
+    const img = new Image()
+    img.onload = () => { getImgUrl.value = true }
+    img.onerror = () => { getImgUrl.value = false }
+    img.src = imgURL
+  }
+}, { immediate: true })
 
 </script>
 
